9ce93b1700b65eb6b7dd151eb0217b78b5cfe16f
Дмитрий Б <dimon20.07@mail.ru>
2023-05-29 18:12:32 +0300

    .\homework2.java

---
+++ homework2.java
@@ -0,0 +1,26 @@
+public class homework2 {
+    Node head;
+    Node tail;
+
+    public void revert(){
+        Node currentNode = head;
+        while(currentNode != null) {
+            Node next = currentNode.next;
+            Node previous = currentNode.previous;
+            currentNode.next = previous;
+            currentNode.previous = next;
+            if (previous == null) {
+                tail = currentNode;
+            }
+            if (next == null) {
+                head = currentNode;
+            }
+            currentNode = next;
+        }
+    }
+    public class Node {
+        int value;
+        Node next;
+        Node previous;
+    }
+}
